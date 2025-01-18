20274bedecf2e589e66082d114f6afef59eb5ec0
Prannaya <prannayagupta@gmail.com>
2025-01-18 13:10:47 +0800

    feat: pls make this work god pls pls

---
+++ git/police/rules/core.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+class Rule(ABC):
+    def __init__(self, name: str, description: str):
+        self.name = name
+        self.description = description
+        
+    @abstractmethod
+    def __call__(self, message: str) -> tuple[boolean, str]:
+        pass
