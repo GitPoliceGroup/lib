373a5d019b3c04a1ae561ce8659ff952d87fabb1
dillionlim <dillionlim2004@gmail.com>
2025-01-18 12:48:10 +0800

    feat: haiku detector

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import typer
 from commands.hello import hello as commandhello
 from commands.goodbye import goodbye as commandgoodbye
+from commands.haiku import check_haiku as commandhaiku
 from utils.camera import start_camera
 
 app = typer.Typer()
@@ -16,6 +17,11 @@ def goodbye(name: str, formal: bool = False):
 @app.command()
 def start_cam():
     start_camera()
+
+@app.command()
+def check_haiku(string: str):
+    # print(bool(commandhaiku(string)))
+    return bool(commandhaiku(string))
 
 if __name__ == "__main__":
     app()
