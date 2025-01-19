9ea229b57f1eb2fc0a7b78ba5aa2ae1fb8edcff0
Nicholas Lee <92599934+LWJ-Nicholas@users.noreply.github.com>
2025-01-19 10:35:18 +0800

    Update __init__.py

--- git_police/tasks/Chess/__init__.py
+++ git_police/tasks/Chess/__init__.py
@@ -8,15 +8,12 @@ from git_police.tasks.core import Task, TaskOutput
 
 class ChessPuzzleTask(Task):
     def __init__(self):
-        self.data = pd.read_csv("git_police/tasks/Chess/puzzle_database.csv")
+        self.db_path = "git_police/tasks/Chess/puzzle_database.csv"
 
     def __call__(self):
-        while True:
-            try:
-                ChessBoardWindow(self.data)
-                return True
-            except:
-                continue
+        ChessBoardWindow(self.db_path)
+        return True
+
 
 if __name__ == "__main__":
     print("Running ChessPuzzleTask...")
