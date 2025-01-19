840dc4d8de4a37acaa6e46ba9ec2032b1823addc
Prannaya <prannayagupta@gmail.com>
2025-01-19 08:30:51 +0800

    feat: modify fastapi server

--- git_police/fastapi/fastapi.py
+++ git_police/fastapi/fastapi.py
@@ -36,46 +36,49 @@ augmenter_list = {
 # Create a FastAPI application
 app = FastAPI()
 
-@app.get("/random_n_rules")
-def random_n_rules(n: int = 4):
+@app.get("/rules")
+def random_n_rules():
+    n = random.randint(3, 5)
     rule_options = list(rule_list.keys())
     rules_implemented = random.sample(rule_options, k=min(n, len(rule_options)))
-    return {"array": rules_implemented}
+    return rules_implemented
 
-@app.post("/check_rule")
-def check_rule(name: str, message: str):
-    if name not in rule_list:
+@app.get("/check_rule")
+def check_rule(rule: str, message: str):
+    if rule not in rule_list:
         return {"success": False, "message": "Rule not found"}
-    result = rule_list[name](message)
+    result = rule_list[rule](message)
     return {
         "success": result.success,
         "message": result.message
     }
 
-@app.get("/random_n_games")
-def random_n_games(n: int = 2):
+@app.get("/games")
+def random_n_games():
+    n = random.randint(1, 3)
     task_options = list(task_list.keys())
     tasks_implemented = random.sample(task_options, k=min(n, len(task_options)))
-    return {"array": tasks_implemented}
+    return tasks_implemented
 
-@app.post("/play_game")
-def play_game(name: str):
-    if name not in task_list:
+@app.get("/play_game")
+def play_game(game: str):
+    if game not in task_list:
         return {"success": False, "message": "Game not found"}
-    result = task_list[name]()
+    result = task_list[game]()
     return {"success": result}
 
-@app.get("/random_n_augs")
-def random_n_augs(n: int = 3):
+@app.get("/augs")
+def random_n_augs():
+    n = random.randint(1, 3)
     augmenter_options = list(augmenter_list.keys())
     augmenters_implemented = random.sample(augmenter_options, k=min(n, len(augmenter_options)))
-    return {"array": augmenters_implemented}
+    return augmenters_implemented
 
-@app.post("/augment")
-def augment(name: str, message: str):
-    if name not in augmenter_list:
+@app.get("/augment")
+def augment(aug: str, message: str):
+    if aug not in augmenter_list:
         return {"success": False, "message": "Augmenter not found"}
-    augmented_message = augmenter_list[name]().augment(message)
+    augmented_message = augmenter_list[aug]().augment(message)
     return {"success": True, "message": augmented_message}
 
 @app.post("/play_emotion")
