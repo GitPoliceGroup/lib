133931c394b118010b0ad0b30fbf00a2a13e0ea8
Prannaya <prannayagupta@gmail.com>
2025-01-19 10:16:52 +0800

    FastApi + VS

--- git_police/fastapi/fastapi.py
+++ git_police/fastapi/fastapi.py
@@ -7,6 +7,7 @@ from git_police.rules import (
 from git_police.tasks import (coding_tips, insultor, fake_error, trivia_generator, chess_puzzle)
 from git_police.augmenters import (AntonymAugmenter, DrunkTypingAugmenter, GermanAugmenter, PirateAugmenter, YodaSpeakAugmenter)
 from git_police.happiness import HappinessChecker
+from pydantic import BaseModel
 
 rule_list = {
     "haiku": check_haiku,
@@ -33,28 +34,33 @@ augmenter_list = {
     "yoda": YodaSpeakAugmenter,
 }
 
+class APIOutput(BaseModel):
+    success: bool
+    message: str | None = None
+
 # Create a FastAPI application
 app = FastAPI()
 
 @app.get("/rules")
-def random_n_rules():
+def random_n_rules() -> list[str]:
     n = random.randint(3, 5)
     rule_options = list(rule_list.keys())
     rules_implemented = random.sample(rule_options, k=min(n, len(rule_options)))
     return rules_implemented
 
+
 @app.get("/check_rule")
-def check_rule(rule: str, message: str):
+def check_rule(rule: str, message: str) -> APIOutput:
     if rule not in rule_list:
-        return {"success": False, "message": "Rule not found"}
+        return BaseModel(**{"success": False, "message": "Rule not found"})
     result = rule_list[rule](message)
-    return {
+    return BaseModel({
         "success": result.success,
         "message": result.message
-    }
+    })
 
 @app.get("/games")
-def random_n_games():
+def random_n_games() -> list[str]:
     n = random.randint(1, 3)
     task_options = list(task_list.keys())
     tasks_implemented = random.sample(task_options, k=min(n, len(task_options)))
